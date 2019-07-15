0f230d94acef5503f997665579e9f54ec07806a6
Ashishfc <ashishs@freightcrate.in>
2019-07-15 17:31:35 +0530

    migration, migrate, postgreSQL </> Mon, 2019-07-15, 17:31:35 IST 196

--- projtest/projtest/settings.py
+++ projtest/projtest/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'calc',
+    'webs',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'projtest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'webs',
+        'USER': 'postgres',
+        'PASSWORD': 'ashishPGS',
+        'HOST': 'localhost',
     }
 }
 
--- projtest/webs/urls.py
+++ projtest/webs/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+# from . import static_content_views as scv
 
 urlpatterns = [
+    # path('index', scv.index, name='index'),
     path('index', views.index, name='index'),
 ]
