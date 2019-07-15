b906b2e0ba94df77dbd69bab2243f97cc2a5919c
Ashishfc <ashishs@freightcrate.in>
2019-07-15 12:29:21 +0530

    load-content-to-static-file </> Mon, 2019-07-15, 12:29:21 IST 196

--- projtest/webs/views.py
+++ projtest/webs/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
+from .models import Destination
+
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
+    dest1 = Destination()
+    dest1.name = 'Mumbai'
+    dest1.desc = 'The city that never sleeps..'
+    dest1.img = 'destination_1.jpg'
+    dest1.price = 700
+
+    dest2 = Destination()
+    dest2.name = 'Hydrabad'
+    dest2.desc = 'First Biryani, Then Sherwani'
+    dest2.img = 'destination_2.jpg'
+    dest2.price = 450
+
+    dest3 = Destination()
+    dest3.name = 'Bengaluru'
+    dest3.desc = 'My kind of IT world'
+    dest3.img = 'destination_3.jpg'
+    dest3.price = 1100
+
+    # create lists
+    dests = [dest1, dest2, dest3]
+
+    return render(request, 'index.html', {'datasets': dests, 'eMailId': 'test@g.com'})
