b00358326b8b9e4884537f32d125f66c465d7490
Ashishfc <ashishs@freightcrate.in>
2019-07-17 11:03:36 +0530

    user-registration-2 </> Wed, 2019-07-17, 11:03:36 IST 198

--- projtest/accounts/views.py
+++ projtest/accounts/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
-from django.contrib.auth.model import User, auth
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+
 
 # Create your views here.
 
 def register(request):
-
     if request.method == 'POST':
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
@@ -12,5 +12,27 @@ def register(request):
         email = request.POST['email']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
+
+        # Validate user entered password correct or not
+        if password1 == password2:
+            print('Password not matching..!')
+
+        # Validate user username
+        elif User.objects.filter(username=user_name).exists():
+            print('User name already exist. Enter unique user name, Try again!')
+
+        # Validate user eMail
+        elif User.objects.filter(email=email).exists():
+            print('Email ID already exist. Enter unique Email ID, Try again!')
+
+        # Save user data
+        else:
+            user = User.objects.create_user(username=user_name, password=password1, email=email, first_name=first_name,
+                                            last_name=last_name)
+            user.save()
+            print('User Created')
+
+        return redirect('/accounts/register')
+
     else:
         return render(request, 'register.html')
--- projtest/projtest/settings.py
+++ projtest/projtest/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'calc',
     'webs',
+    'accounts',
 ]
 
 MIDDLEWARE = [
